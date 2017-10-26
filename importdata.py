0fdc60ac03214217b8eb09c7d2a5b15d928af5f4
Najim Mazidi <nmazidi95@gmail.com>
2017-10-26 13:52:48 +0100

    importing both files and storing in seperate arrays

--- importdata.py
+++ importdata.py
@@ -1,17 +1,37 @@
 import matplotlib.pyplot as plt
 
-raw = []
-x = []
-y = []
+rawtrain = []
+rawtest = []
+trainX = []
+trainY = []
+testX = []
+testY = []
 
 with open("TRAIN10X.DAT") as file:
     for line in file:
-        raw.append(line.rstrip('\n\r '))
+        rawtrain.append(line.rstrip('\n\r '))
+with open("TEST10X.DAT") as file:
+    for line in file:
+        rawtest.append(line.rstrip('\n\r '))
 i = 0
-while i < len(raw):
-    if raw[i] != '':
-        x.append(raw[i])
-        y.append(raw[i+2])
+while i < len(rawtrain):
+    if rawtrain[i] != '':
+        trainX.append(rawtrain[i])
+        trainY.append(rawtrain[i+2])
         i += 5
-print(x)
-print(y)
+i = 0
+while i < len(rawtest):
+    if rawtest[i] != '':
+        testX.append(rawtest[i])
+        testY.append(rawtest[i+2])
+        i += 5
+
+
+print('Train X:')
+print(trainX)
+print('Train Y:')
+print(trainY)
+print('Test X:')
+print(testX)
+print('Test Y:')
+print(testY)
