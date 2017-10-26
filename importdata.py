c7d8a81aedcdb545488a00508bcce154b081b890
Najim Mazidi <nmazidi95@gmail.com>
2017-10-26 13:44:15 +0100

    data import done

--- importdata.py
+++ importdata.py
@@ -1,3 +1,17 @@
-f = open("TRAIN10X.DAT", "r")
+import matplotlib.pyplot as plt
 
-print(f.readline())
+raw = []
+x = []
+y = []
+
+with open("TRAIN10X.DAT") as file:
+    for line in file:
+        raw.append(line.rstrip('\n\r '))
+i = 0
+while i < len(raw):
+    if raw[i] != '':
+        x.append(raw[i])
+        y.append(raw[i+2])
+        i += 5
+print(x)
+print(y)
