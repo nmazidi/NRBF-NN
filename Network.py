7839b6c2552e74e524f8f525a325d6769188400b
Najim Mazidi <nmazidi95@gmail.com>
2017-11-07 23:22:34 +0000

    Update oop functionality

--- Network.py
+++ Network.py
@@ -7,3 +7,5 @@ class Network(object):
 
     def createNode(self, center, init_weight):
         self.hidden_nodes.append(Neuron(center,init_weight,self.sigma))
+    def updateNode(self, node_id, new_weight):
+        self.hidden_nodes[node_id].weight = new_weight
